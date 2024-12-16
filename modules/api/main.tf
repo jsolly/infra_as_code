c2865ad1962639190438ec05ee3222e4609231b9
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-15 19:12:46 -0500

    .

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -1,43 +1,11 @@
-resource "aws_s3_bucket" "storage_bucket" {
-  bucket        = var.storage_bucket_name
-  force_destroy = true
+module "storage" {
+  source = "./storage"
+
+  storage_bucket_name = var.storage_bucket_name
 }
 
-resource "aws_s3_bucket_public_access_block" "storage_bucket" {
-  bucket = aws_s3_bucket.storage_bucket.id
+module "metadata" {
+  source = "./metadata"
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "storage_bucket" {
-  bucket = aws_s3_bucket.storage_bucket.id
-
-  rule {
-    id     = "delete_old_objects"
-    status = "Enabled"
-
-    expiration {
-      days = 30
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "metadata_table" {
-  name           = var.metadata_table_name
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "date"
-  stream_enabled = true
-
-  attribute {
-    name = "date"
-    type = "S"
-  }
-
-  ttl {
-    attribute_name = "expiration_time"
-    enabled        = true
-  }
+  metadata_table_name = var.metadata_table_name
 }
