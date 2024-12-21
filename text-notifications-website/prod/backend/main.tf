ace961e42a7e3ae83f78397591360095b42eee90
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-21 12:28:18 -0500

    wip

--- text-notifications-website/prod/backend/main.tf
+++ text-notifications-website/prod/backend/main.tf
@@ -13,100 +13,99 @@ module "lambda_code_storage" {
   asset_storage_bucket = var.lambda_code_bucket
 }
 
-# resource "aws_iam_role" "lambda_role" {
-#   name = "${var.photo_fetcher_name}-role"
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.photo_fetcher_name}-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "lambda.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
 
-# resource "aws_iam_role_policy" "s3_access" {
-#   name = "${var.photo_fetcher_name}-s3-access"
-#   role = aws_iam_role.lambda_role.id
+resource "aws_iam_role_policy" "s3_access" {
+  name = "${var.photo_fetcher_name}-s3-access"
+  role = aws_iam_role.lambda_role.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "s3:PutObject",
-#           "s3:GetObject"
-#         ]
-#         Resource = ["${module.asset_storage.bucket_arn}/*"]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject"
+        ]
+        Resource = ["${module.asset_storage.bucket_arn}/*"]
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy" "dynamodb_access" {
-#   name = "${var.photo_fetcher_name}-dynamodb-access"
-#   role = aws_iam_role.lambda_role.id
+resource "aws_iam_role_policy" "dynamodb_access" {
+  name = "${var.photo_fetcher_name}-dynamodb-access"
+  role = aws_iam_role.lambda_role.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "dynamodb:PutItem",
-#           "dynamodb:GetItem"
-#         ]
-#         Resource = [module.metadata.table_arn]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem"
+        ]
+        Resource = [module.metadata.table_arn]
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "lambda_basic" {
-#   role       = aws_iam_role.lambda_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-# resource "aws_lambda_function" "nasa_photo_fetcher" {
-#   s3_bucket     = var.lambda_code_bucket
-#   s3_key        = var.lambda_code_key
-#   function_name = var.photo_fetcher_name
-#   role          = aws_iam_role.lambda_role.arn
-#   handler       = var.function_handler
-#   runtime       = var.runtime
-#   timeout       = var.function_timeout
-#   memory_size   = var.function_memory_size
+resource "aws_lambda_function" "nasa_photo_fetcher" {
+  s3_bucket     = var.lambda_code_bucket
+  s3_key        = var.lambda_code_key
+  function_name = var.photo_fetcher_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = var.function_handler
+  runtime       = var.runtime
+  timeout       = var.function_timeout
+  memory_size   = var.function_memory_size
 
-#   environment {
-#     variables = {
-#       NASA_API_KEY              = var.nasa_api_key
-#       ASSET_STORAGE_BUCKET = module.asset_storage.bucket_name
-#       METADATA_TABLE_NAME       = module.metadata.table_name
-#     }
-#   }
-# }
+  environment {
+    variables = {
+      NASA_API_KEY        = var.nasa_api_key
+      METADATA_TABLE_NAME = module.metadata.table_name
+    }
+  }
+}
 
-# resource "aws_cloudwatch_event_rule" "daily_trigger" {
-#   name                = "${var.photo_fetcher_name}-daily-trigger"
-#   description         = "Triggers the NASA photo fetcher Lambda function daily at 1 AM"
-#   schedule_expression = "cron(0 1 * * ? *)" # Runs at 1 AM UTC daily
-# }
+resource "aws_cloudwatch_event_rule" "daily_trigger" {
+  name                = "${var.photo_fetcher_name}-daily-trigger"
+  description         = "Triggers the NASA photo fetcher Lambda function daily at 1 AM"
+  schedule_expression = "cron(0 1 * * ? *)" # Runs at 1 AM UTC daily
+}
 
-# resource "aws_cloudwatch_event_target" "lambda_target" {
-#   rule      = aws_cloudwatch_event_rule.daily_trigger.name
-#   target_id = "TriggerNASAPhotoFetcher"
-#   arn       = aws_lambda_function.nasa_photo_fetcher.arn
-# }
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.daily_trigger.name
+  target_id = "TriggerNASAPhotoFetcher"
+  arn       = aws_lambda_function.nasa_photo_fetcher.arn
+}
 
-# resource "aws_lambda_permission" "allow_eventbridge" {
-#   statement_id  = "AllowEventBridgeInvoke"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.nasa_photo_fetcher.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.daily_trigger.arn
-# }
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowEventBridgeInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.nasa_photo_fetcher.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily_trigger.arn
+}
 
