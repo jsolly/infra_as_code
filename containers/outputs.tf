8e4746c653cd7fe13915b9513df716f4c64c2694
John Solly <9572232+jsolly@users.noreply.github.com>
2025-02-23 14:12:49 -0500

    .

--- containers/outputs.tf
+++ containers/outputs.tf
@@ -2,3 +2,8 @@ output "repository_arn" {
   description = "The ARN of the repository"
   value       = aws_ecr_repository.function.arn
 }
+
+output "repository_url" {
+  description = "The URL of the repository"
+  value       = aws_ecr_repository.function.repository_url
+}
