5a1e57579dcba6a21aa699bd5acc606a1602851a
John Solly <9572232+jsolly@users.noreply.github.com>
2025-02-23 18:18:57 -0500

    .

--- validators/outputs.tf
+++ validators/outputs.tf
@@ -1,10 +1,10 @@
 output "site_key" {
   description = "The site key for the Turnstile widget"
-  value       = cloudflare_turnstile_widget.widget.site_key
+  value       = cloudflare_turnstile_widget.widget.id
 }
 
 output "secret_key" {
   description = "The secret key for the Turnstile widget"
-  value       = cloudflare_turnstile_widget.widget.secret_key
+  value       = cloudflare_turnstile_widget.widget.secret
   sensitive   = true
 }
